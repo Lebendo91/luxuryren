b6b8369b26060056a610e4be05f3513b3b8c5388
Lebendo91 <ibrahim.boughalem06@gmail.com>
2026-01-04 20:59:39 +0100

    Final: Fix type error for Car model

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -27,7 +27,7 @@ export default async function CarsPage() {
             </div>
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-                {cars.map((car: Car) => (
+                {cars.map((car: any) => (
                     <CarCard key={car.id} car={car} />
                 ))}
             </div>
